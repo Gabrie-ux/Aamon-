b6632745a4ea197625aef4418a92d2ee7f488af4
Wichi <sebastianbarbaro82@gmail.com>
2025-03-30 11:03:07 -0400

    Actualizar group-ver.js

--- plugins/group-ver.js
+++ plugins/group-ver.js
@@ -1,25 +1,22 @@
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn }) => {
-    // Verifica si hay un mensaje citado y si es una imagen
-    if (!m.quoted || !m.quoted.image) {
-        return conn.reply(m.chat, '❌ Por favor, envía una imagen y luego usa el comando para verla.', m);
-    }
-
-    // Verifica si la imagen ya ha sido vista
-    if (m.quoted.viewed) {
-        return conn.reply(m.chat, '❌ Esta imagen ya ha sido vista y no se puede volver a ver.', m);
-    }
-
-    // Marca la imagen como vista
-    m.quoted.viewed = true;
-
-    // Descarga la imagen y envíala al chat
-    const imgBuffer = await conn.downloadMediaMessage(m.quoted);
-    
-    await conn.sendMessage(m.chat, { image: imgBuffer, caption: 'Aquí está la imagen que solicitaste.' }, { quoted: m });
+const handler = async (m, { conn }) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!mime) throw 'No se detectó un archivo válido';
+  const media = await q.download();
+  try {
+    const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+    const link = await (isTele ? uploadImage : uploadFile)(media);
+    await conn.sendFile(m.chat, link, '', '© Prohibido la copia, Código Oficial de Aamon™', m);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-handler.command = ['ver']; // Puedes cambiar esto por cualquier otro comando que prefieras
-handler.group = true; // Esto permite que el comando funcione en grupos
-
+handler.help = ['ver'];
+handler.tags = ['herramientas'];
+handler.command = /^(ver)$/i;
 export default handler;
