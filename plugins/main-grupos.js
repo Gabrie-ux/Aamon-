16bf9448f4efca537c424f14125e581fb2ad4005
Wichi <sebastianbarbaro82@gmail.com>
2025-03-29 20:36:07 -0400

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,26 +1,25 @@
-import fetch from 'node-fetch'
-let handler  = async (m, { conn, usedPrefix, command }) => {
 
-let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad* ⭐
+import fetch from 'node-fetch';
 
-1-Aamon
-*✰* https://chat.whatsapp.com/FeWfraCmhn0BFtRatwm7oM
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let grupos = "*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad* ⭐\n\n" +
+                 "1-Aamon\n" +
+                 "*✰* https://chat.whatsapp.com/FeWfraCmhn0BFtRatwm7oM\n\n" +
+                 "*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*\n\n" +
+                 "➠ Enlace anulado? entre aquí! \n\n" +
+                 "⭐ Canal :\n" +
+                 "*✰* https://whatsapp.com/channel/0029Vb5mi8y3wtb4XeFy8i2i\n\n" +
+                 "> By Aamon";
 
-*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*
+    // Asegúrate de definir 'imagen2' correctamente antes de usarlo
+    let imagen2 = 'https://qu.ax/fPklC.jpg';
 
-➠ Enlace anulado? entre aquí! 
-
-⭐ Canal :
-*✰* https://whatsapp.com/channel/0029Vb5mi8y3wtb4XeFy8i2i
-
-> By Aamon`
-
-await conn.sendFile(m.chat, imagen2, "ian.jpg", grupos, m, null, rcanal)
-
-await m.react(emojis)
-
+    await conn.sendFile(m.chat, imagen2, "ian.jpg", grupos, m, null, rcanal);
+    await m.react(emojis);
 }
-handler.help = ['grupos']
-handler.tags = ['main']
-handler.command = ['grupos', 'iangrupos', 'gruposian']
-export default handler
+
+handler.help = ['grupos'];
+handler.tags = ['main'];
+handler.command = ['grupos', 'iangrupos', 'gruposian'];
+
+export default handler;
